14b84c82cbadc8e9afd9cd4f7f8afc704fc0b4b4
kimihito <tatsurotamashiro@gmail.com>
2020-07-04 14:51:33 +0900

    Port web dir

--- web2/src/Main.elm
+++ web2/src/Main.elm
@@ -1,56 +1,105 @@
-module Main exposing (..)
-
+module Main exposing (main)
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Http
+import Html exposing (Html, div, text, h2, table, tr, td)
+import Json.Decode exposing (Decoder, field, string)
 
+type alias Dog
+    = { id : String
+      , breed : String
+      , place : String
+      }
 
----- MODEL ----
-
-
-type alias Model =
-    {}
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
-
-
-
----- UPDATE ----
-
-
-type Msg
-    = NoOp
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
-
-
-
----- VIEW ----
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        ]
-
-
-
----- PROGRAM ----
-
+-- MAIN
 
 main : Program () Model Msg
 main =
     Browser.element
-        { view = view
-        , init = \_ -> init
+        { init = init
+        , view = view
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
+
+-- MODEL
+type Model
+  = Failure
+  | Loading
+  | Success (List Dog)
+
+init : () -> (Model, Cmd Msg)
+init _ =
+    (Loading, getDogs)
+
+
+-- UPDATE
+
+type Msg
+    = GotDogs (Result Http.Error (List Dog))
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+       GotDogs result ->
+        case result of
+            Ok dogs ->
+                (Success dogs, Cmd.none)
+            Err _ ->
+                (Failure, Cmd.none)
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h2 [] [ text "dogs"]
+        , viewDogs model
+        ]
+
+viewDogs : Model -> Html Msg
+viewDogs model =
+    case model of
+        Failure ->
+            div []
+                [ text "I could not load dogs." ]
+
+        Loading ->
+            text "loading..."
+
+        Success dogs ->
+            table []
+                (List.map viewDog dogs)
+
+viewDog : Dog -> Html Msg
+viewDog dog =
+    tr []
+        [
+             td []
+                [ text dog.breed ]
+        ,    td []
+                [ text dog.place ]
+        ,    td []
+                [ text dog.id ]
+        ]
+
+
+-- HTTP
+getDogs : Cmd Msg
+getDogs =
+    Http.get
+        { url = "https://kimihito.github.io/okinaniwel/dogs.json"
+        , expect = Http.expectJson GotDogs ( Json.Decode.list dogDecoder )
+        }
+
+dogDecoder : Decoder Dog
+dogDecoder =
+    Json.Decode.map3 Dog
+        ( field "id" string)
+        ( field "bread" string)
+        ( field "place" string)
