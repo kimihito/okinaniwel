711d794eebf5e9ff5f9119bcb261584452b8e05c
kimihito <kimihito@users.noreply.github.com>
2020-07-04 21:22:17 +0000

    deploy: 2b85664014058b8110a4bcf05cf550237099f77c

---
+++ precache-manifest.4b588cb5815adccdeabbce977669fece.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/okinaniwel/favicon.ico"
+  },
+  {
+    "revision": "d430d45f81daa40f3665d72bad466c5c",
+    "url": "/okinaniwel/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/okinaniwel/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/okinaniwel/manifest.json"
+  },
+  {
+    "revision": "f9f6e2b987646dbe8fdc",
+    "url": "/okinaniwel/static/css/vendors~main.8bbe462c.chunk.css"
+  },
+  {
+    "revision": "d88ad990b5c4e1b0a937",
+    "url": "/okinaniwel/static/js/main.d88ad990.chunk.js"
+  },
+  {
+    "revision": "944c76979cfcf874f1fa",
+    "url": "/okinaniwel/static/js/runtime~main.944c7697.js"
+  },
+  {
+    "revision": "f9f6e2b987646dbe8fdc",
+    "url": "/okinaniwel/static/js/vendors~main.f9f6e2b9.chunk.js"
+  }
+]);
